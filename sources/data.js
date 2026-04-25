01c4a5f7e9362807a13910af7338afcc35efba62
conorwhanson <condorhanson@gmail.com>
2026-04-25 11:57:33 -0500

    moved data, broke variables into species, added icon attribution

--- sources/data.js
+++
@@ -1,3 +0,0 @@
-var stops = {"Location1": 43.5467, "Location2": -93.8736};
-
-var ft = {};
---
+++ sources/js/data.js
@@ -0,0 +1,5 @@
+var stops = {"Location1": [43.5467, -93.8736], "Location2": [45.999, -92.888]};
+
+var species1 = {};
+var species2 = {};
+var species3 = {};
